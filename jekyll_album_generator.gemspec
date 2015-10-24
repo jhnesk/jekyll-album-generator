de32751173ef1ddc57b15ca1022a14160c0c16f4
Johan Ask <jhnesk@gmail.com>
2015-10-24 18:25:04 +0200

    Project structure and plugin stub

---
+++ jekyll_album_generator.gemspec
@@ -0,0 +1,11 @@
+Gem::Specification.new do |spec|
+	spec.name        = 'jekyll_album_generator'
+	spec.version     = '0.0.1'
+	spec.summary     = 'Jekyll Album Generator'
+	spec.description = 'Jekyll plugin for generating photo album data'
+	spec.authors     = ["Johan Ask"]
+	spec.email       = 'jhnesk@gmail.com'
+	spec.homepage    = 'https://github.com/jhnesk/jekyll-album-generator'
+	spec.files       = ["lib/album_generator.rb"]
+	spec.licenses    = ["GPL-3.0"]
+end
---
+++ lib/album_generator.rb
@@ -0,0 +1,27 @@
+# Copyright 2015 Johan Ask.
+# E-mail: jhnesk@gmail.com
+#
+# This file is part of Jekyll Album Generator.
+#
+# Jekyll Album Generator is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Jekyll Album Generator is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with Jekyll Album Generator. If not, see <http://www.gnu.org/licenses/>.
+#
+module Jekyll
+
+	class AlbumGenerator < Generator
+
+		def generate(site)
+			puts "Generating album data..."
+		end
+	end
+end
